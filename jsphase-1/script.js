1b02eca6bc77ed379831276802f7281aac7e6dc6
Dinesh <dinesh@annantasourcellc.onmicrosoft.com>
2024-06-25 10:24:06 +0530

    basic with exercise

--- jsphase-1/script.js
+++ jsphase-1/script.js
@@ -1,7 +1,9 @@
 /** JavaScript Variables **/
 // Variables are containers for storing Information or Data
 // JavaScript Variables can be declared in 4 ways
-    // Using Var Keyword - function scope or global scope
+    // Using var Keyword - function scope or global scope
+    // Using let Keyword - block scope
+    // Using const Keyword - block scope
 
     // var - accessible outside the block | let & const can't
 
@@ -24,3 +26,28 @@ console.log(a+b+e+x);
 // console.log(c);
 console.log(d);
 // console.log(y);
+
+/* Exercise 1 */
+var price = 700;
+var product = "iPhone20";
+var tax = 77;
+console.log(product);
+
+var total=price+tax;
+console.log(total);
+
+/* Exercise 2 */ 
+var FruitName = "Mango";
+var count = 50;
+var price = 100;
+var total = count * price;
+
+console.log(FruitName);
+console.log("Total Price", total);
+
+
+/** JavaScript Operators **/
+let m = 5;
+let n = m++;
+
+console.log(n);
