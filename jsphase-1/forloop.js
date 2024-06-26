34b83008f9c257eef50d4a69e8ad8258e193bec0
Dinesh <dinesh@annantasourcellc.onmicrosoft.com>
2024-06-26 12:13:21 +0530

    for loop

---
+++ jsphase-1/forloop.js
@@ -0,0 +1,37 @@
+/** JavaScript for loop **/
+/*
+    In JavaScript, a for loop is a control flow statement used to
+    repeatedly execute a block of code based on a specified condition.
+    The general syntax of a for loop in JavaScript is as follows:
+
+    for (initialization: condition; iteration) {
+        ! Code to be executed in each iteration
+    }
+*/ 
+
+for (count=1;count<=5;count=count+1) {
+    console.log(count)
+}
+
+for (i=1;i<=5;i=i+1) {
+    console.log(i) 
+}
+
+for (a=1;a<=5;a=a+2) {
+    console.log(a+"A") 
+}
+
+for (n=5;n>=0;n=n-1){
+    console.log(n)
+}
+
+for (b=1;b<=10;b=b+2){
+    console.log(b)
+}
+
+for (tb=1;tb<=5;tb=tb+1) {
+    console.log("2x"+tb+"="+(tb*2)) 
+}
+
+
+
