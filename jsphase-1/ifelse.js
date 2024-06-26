b84f35bcc9a8920b35ab0fbf3f76ed5354393e01
Dinesh <dinesh@annantasourcellc.onmicrosoft.com>
2024-06-26 10:17:01 +0530

    if else
    If else, logical operator AND(&&) OR(||) NOT(!) < <= > >= == === !==

--- jsphase-1/ifelse.js
+++ jsphase-1/ifelse.js
@@ -60,3 +60,78 @@ if (birthday) {
 } else {
     console.log("Have a great day!")
 }
+
+var soulmate = true
+if (soulmate) {
+    console.log("I love my life <3 Your my everything")
+} else {
+    console.log("I hate you d Rakshasi!")
+}
+
+
+/* Logical */
+// ! AND(&&) operator
+console.log(true && false && true)                  
+console.log(true && true)  
+console.log(true && false)
+
+// ! OR(||)  operator
+console.log(true || false || false)
+
+// ! NOT(!) operator
+console.log(!true)
+
+
+// ! if statement
+var color = "green"
+if (color == "red") {
+    console.log("STOP")
+}
+if( color == "yellow") {
+    console.log("Get Ready")
+}
+if (color == "green") {
+    console.log("Go")
+}
+
+// Exercise
+var season = "autumn"
+if (season == "spring") {
+    console.log("Enjoy the blooming flowers.")
+}
+if (season == "summer") {
+    console.log("Have fun in the sun.")
+}
+if (season == "autumn") {
+    console.log("Admire the colorful leaves.")
+}
+if (season == "winter") {
+    console.log("Bundle up and stay warm.")
+}
+
+// ! if else statement
+var score = 70
+if (score<=50) {
+    console.log("You need to improve.")
+}
+else if (score>50 && score<=70) {
+    console.log("Good Job!")
+}
+else if (score>70) {
+    console.log("Excellent Performance")
+}
+
+// Exercise
+var number = 11
+if (number & 1==1) {
+    console.log("The number is odd")
+} else {
+    console.log("The number is even")
+}
+
+var char = "e"
+if (char == "a" || char == "e" || char == "i" || char == "o" || char == "u") {
+    console.log("It's a vowel.")
+} else {
+    console.log("It's a consonant.")
+}
