b3726b2557f364d788698ab9f85b28d767349b8f
Dinesh <dinesh@annantasourcellc.onmicrosoft.com>
2024-06-26 06:49:34 +0530

    If else

---
+++ jsphase-1/ifelse.js
@@ -0,0 +1,62 @@
+/** JavaScript If else **/
+/*
+    if(condition){
+        // Code to be executed if the condition is true
+    } else {
+        // Code to be executed if the condition is false
+    }
+
+    if (true) {
+        console.log("If is working")
+    } else {
+        console.log("else is working")
+    }
+ */
+
+if(false) {
+    console.log("It's True")
+} else {
+    console.log("It's False")
+}
+
+
+var rain = true
+
+if (rain) {
+    console.log("Take an umbrella.")
+} else {
+    console.log("Enjoy the sunshine.")
+}
+
+
+// Exercise 1
+var homework = true
+if (homework) {
+    console.log("Great Job!")
+} else {
+    console.log("Finish your homework before playing.")
+}
+
+// Exercise 2
+var cookies = true
+if (cookies) {
+    console.log("Would you like a cookie?")
+} else {
+    console.log("Time to bake more cookies.")
+}
+
+// Exercise 3
+var teeth = true
+if (teeth) {
+    console.log("Your teeth are clean.")
+} else {
+    console.log("Go brush your teeth now.")
+}
+
+// Exercise 4
+var birthday = true
+if (birthday) {
+    console.log("Happy birthday! Enjoy your special day!")
+} else {
+    console.log("Have a great day!")
+}
