8f9df4931e110d34110557d26c7f02d1ba81c7e7
Dinesh <dinesh@annantasourcellc.onmicrosoft.com>
2024-06-26 06:24:48 +0530

    Return

---
+++ jsphase-1/return.js
@@ -0,0 +1,29 @@
+/** JavaScript Return Type **/
+/*
+    JavaScript, the return keyword is used within a function to specify the value 
+    that the function should produce as its result or return to the caller. When a 
+    function is executed and encounters a return statement, it immediately stops 
+    executing and returns the specified value to the calling code. 
+
+    Here's how the return keyword works:
+*/
+function myname()
+{
+    return "I'm Back Dinesh"
+}
+
+// myname()
+var a = myname()
+console.log(a)
+
+// Exercise
+function add(a,b)
+{
+    return (a+b)
+}
+
+// var a = add()
+// console.log(a)
+
+var total= add(10,76)
+console.log(total)
