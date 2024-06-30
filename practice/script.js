9a2a69e8c82d93e03b168964efb5c626a6b35694
Dinesh <dinesh@annantasourcellc.onmicrosoft.com>
2024-06-30 06:15:43 +0530

    Practice Day1

---
+++ practice/script.js
@@ -0,0 +1,57 @@
+// var
+var a = "Dinesh "
+var b = "Srilatha"
+
+console.log(a+b)
+
+// Function
+function abc() {                            // ! Function Syntax
+
+    console.log("Hello")                    // ! Say what need to be done. Create your function
+
+}                                           // ! Function Syntax
+abc()                                       // ! Calling Function
+
+function iPhone() {
+    console.log("This is iPhone")
+}
+function oppo() {
+    console.log("This is Oppo")
+}
+function redmi() {
+    console.log("This is Redmi")
+}
+function nokia() {
+    console.log("This is Nokia")
+}
+
+iPhone()
+nokia()
+oppo()
+redmi()
+
+// Add Function
+var aa = 10
+var bb = 20
+
+function add() {
+    console.log(aa+bb)
+}
+
+add()
+
+// Fav
+var fa = "Kamal"
+var fp = "Dhoni"
+var fm = "Anbe Sivam"
+
+console.log("Favourite Movie: "+fm)
+
+function fav() {
+    console.log("Favourite Actor:"+fa)
+    console.log("Favourite Player:"+fp)
+    console.log("Favourite Movie:"+fm)
+}
+
+fav()
+fav()
